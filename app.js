9b75b69293e23a0f3df5396a2102e8eaf319080c
Sarven Capadisli <info@csarven.ca>
2016-09-30 14:07:40 -0400

    Add Updates-Via

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
   }
   res.header("Access-Control-Allow-Headers", "Content-Length, Content-Type, If-None-Match, Link, Location, Origin, Slug, X-Requested-With");
-   res.header("Access-Control-Expose-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Accept-Post, Allow, Content-Length, Content-Type, Link, Last-Modified, ETag, Vary");
+   res.header("Access-Control-Expose-Headers", "Accept-Post, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Allow, Content-Length, Content-Type, ETag, Last-Modified, Link, Updates-Via, Vary");
   return next();
 });
 
