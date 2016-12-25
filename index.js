aa3eb5d8de1e68803724cc0b703ba3bf600590fe
Sarven Capadisli <info@csarven.ca>
2016-12-25 18:23:07 +0100

    Reorder parser/serialization inclusion

--- index.js
+++ index.js
@@ -7,15 +7,15 @@
 
 var LdpStore = require('rdf-store-ldp/lite')
 var SimpleRDF = require('simplerdf')
-var N3Parser = require('rdf-parser-n3')
 var JsonLdParser = require('rdf-parser-jsonld')
+var N3Parser = require('rdf-parser-n3')
 var RdfaParser = require('rdf-parser-rdfa')
 var RdfXmlParser = require('rdf-parser-rdfxml')
 var SimpleRDFParse = require('simplerdf-parse')
 
 var formats = {parsers: {}}
-formats.parsers['text/turtle'] = N3Parser
 formats.parsers['application/ld+json'] = JsonLdParser
+formats.parsers['text/turtle'] = N3Parser
 formats.parsers['application/xhtml+xml'] = RdfaParser
 formats.parsers['text/html'] = RdfaParser
 formats.parsers['application/rdf+xml'] = RdfXmlParser
@@ -23,8 +23,8 @@ var parser = SimpleRDFParse(formats.parsers)
 SimpleRDF.parse = parser.parse.bind(parser)
 
 var storeFormats = {parsers:{}}
-storeFormats.parsers['text/turtle'] = N3Parser
 storeFormats.parsers['application/ld+json'] = JsonLdParser
+storeFormats.parsers['text/turtle'] = N3Parser
 storeFormats.parsers['application/xhtml+xml'] = RdfaParser
 storeFormats.parsers['text/html'] = RdfaParser
 storeFormats.parsers['application/rdf+xml'] = RdfXmlParser
