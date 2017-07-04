6ce46d0750351843ebc22b2979414565a7f9eb4f
Sarven Capadisli <info@csarven.ca>
2017-07-04 22:15:08 +0200

    Allow http(s) only

--- index.js
+++ index.js
@@ -201,7 +201,8 @@ console.log(config);
 
       var origin = req.header('Origin');
       if(origin) {
-        if(origin.trim().toLowerCase() != 'null' && origin.trim().toLowerCase() != 'undefined') {
+        var o = origin.trim().toLowerCase();
+        if(o != 'null' && o != 'undefined' && !o.startsWith('http:') && !o.startsWith('https:')) {
           res.header("Access-Control-Allow-Origin", req.header('Origin'));
           res.header("Access-Control-Allow-Credentials", "true");
         }
