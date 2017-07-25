623b4fd292770378765f25574a33c137b7f050c1
Sarven Capadisli <info@csarven.ca>
2017-07-25 19:23:33 +0200

    Use route if only proxyPath is configured

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ function config(configFile){
   config['reportsPath'] = config.reportsPath || 'reports/';
   config['maxPayloadSize'] = config.maxPayloadSize || 100000;
   config['maxResourceCount'] = config.maxResourceCount || 100;
-  config['proxyPath'] = config.proxyPath || config['authority'] + '/proxy';
+  config['proxyPath'] = config.proxyPath || '/proxy';
 
   var createDirectories = [config['inboxPath'], config['queuePath'], config['annotationPath'], config['reportsPath']];
   createDirectories.forEach(function(path){ if(!fs.existsSync(path)){ fs.mkdirSync(path); } });
@@ -296,33 +296,35 @@ console.log(config);
       return next();
     });
 
-    app.use(config.proxyPath, function(req, res, next){
-      switch(req.method){
-        case 'GET': case 'HEAD': case 'OPTIONS':
-          break;
-        default:
-          res.status(405);
+    if(config.proxyPath.length > 0) {
+      app.use(config.proxyPath, function(req, res, next){
+        switch(req.method){
+          case 'GET': case 'HEAD': case 'OPTIONS':
+            break;
+          default:
+            res.status(405);
+            res.set('Allow', 'GET, HEAD, OPTIONS');
+            res.end();
+            return next();
+            break;
+        }
+
+        if(req.query && 'uri' in req.query && req.query.uri.length > 0 && req.query.uri.indexOf(config.hostname) == -1 && req.query.uri.indexOf(config.hostname) == '128.0.0.1'){
+            var rO = {
+              'method': req.method,
+              'uri': req.query.uri,
+              headers: {'Accept': req.headers['accept']}
+            };
+            request(rO).pipe(res);
+        }
+        else {
+          res.status(400);
           res.set('Allow', 'GET, HEAD, OPTIONS');
           res.end();
           return next();
-          break;
-      }
-
-      if(req.query && 'uri' in req.query && req.query.uri.length > 0 && req.query.uri.indexOf(config.hostname) == -1 && req.query.uri.indexOf(config.hostname) == '128.0.0.1'){
-          var rO = {
-            'method': req.method,
-            'uri': req.query.uri,
-            headers: {'Accept': req.headers['accept']}
-          };
-          request(rO).pipe(res);
-      }
-      else {
-        res.status(400);
-        res.set('Allow', 'GET, HEAD, OPTIONS');
-        res.end();
-        return next();
-      }
-    });
+        }
+      });
+    }
 
     var oR = (options && options.omitRoutes) ? '|' + options.omitRoutes.join('|') : '';
     var handleRoutes = new RegExp('^(?!/index.html' + oR + ').*$');
