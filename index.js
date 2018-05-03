8d45737b08efbc8e573a0de1a239703f8685d7a0
Stian Soiland-Reyes <stain@apache.org>
2018-05-03 18:22:20 +0100

    Using prescribed Apache license header

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 /** mayktso
  *
- * Sarven Capadisli <info@csarven.ca> http://csarven.ca/#i
- * http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
  * https://github.com/csarven/mayktso
+ *
+ * Copyright 2016 Sarven Capadisli <info@csarven.ca> http://csarven.ca/#i
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 var LdpStore = require('rdf-store-ldp/lite')
